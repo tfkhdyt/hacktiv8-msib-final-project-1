51a8bdc1ebb1a5125a042361daaf65466892c630
tfkhdyt <tfkhdyt@proton.me>
2023-04-06 18:35:18 +0700

    feat: create /todos route

--- handler/app.go
+++ handler/app.go
@@ -2,6 +2,9 @@ package handler
 
 import (
 	"hacktiv8-msib-final-project-1/database"
+	"hacktiv8-msib-final-project-1/handler/http_handler"
+	"hacktiv8-msib-final-project-1/repository/todo_repository/todo_pg"
+	"hacktiv8-msib-final-project-1/service"
 	"log"
 	"os"
 
@@ -17,6 +20,11 @@ func StartApp() {
 	r := gin.Default()
 
 	db := database.GetPostgresInstance()
+	todoRepo := todo_pg.NewTodoPG(db)
+	todoService := service.NewTodoService(todoRepo)
+	todoHandler := http_handler.NewTodoHandler(todoService)
+
+	r.POST("/todos", todoHandler.CreateTodo)
 
 	log.Fatalln(r.Run(":" + PORT))
 }
